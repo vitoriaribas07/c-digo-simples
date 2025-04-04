cb597478afcc186020837edae17820a36983606e
vitoriaribas07 <vitoriaribas274@gmail.com>
2025-04-04 09:11:54 -0300

    Create Par.py

---
+++ Par.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:  # Condição
+    print("O número é par.")  # Executa se a condição for verdadeira
+else:
+    print("O número é ímpar.")  # Executa se a condição for falsa
